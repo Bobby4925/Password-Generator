7e4ced9eaa08d7b32765ea1305694e98efb66a5b
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-05 18:04:06 -0700

    Added Parsing of Input

--- generator/Program.cs
+++ generator/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string pswd = ""; 
-var a = Convert.ToChar("A"); 
-Console.WriteLine((int)a); 
+Console.WriteLine("Welcome to this Password Generator");
+Console.WriteLine("Before We Begin What Size would you like your password to be?");
+string input = Console.ReadLine(); 
+int length; 
+if(!Int32.TryParse(input, out length)){
+    throw new Exception("You must enter a valid integer"); 
+}
+else{
+    length = int.Parse(input);
+}
+int arrLength = length + 1; 
+string[] passwordArr = new string[arrLength]; 
